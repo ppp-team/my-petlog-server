f9322edf103ae2aa431956bfe4c10eb6ae646865
haden <80521474+thdefn@users.noreply.github.com>
2024-01-25 16:48:03 +0900

    chore: 베이스 엔티티 필드명 수정 - 협의한 erd 에 맞게 베이스 엔티티의 필드명을 수정함

--- petlog-domain/src/main/java/com/ppp/domain/common/BaseTimeEntity.java
+++ petlog-domain/src/main/java/com/ppp/domain/common/BaseTimeEntity.java
@@ -17,5 +17,5 @@ public abstract class BaseTimeEntity {
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private LocalDateTime modifiedAt;
 }
