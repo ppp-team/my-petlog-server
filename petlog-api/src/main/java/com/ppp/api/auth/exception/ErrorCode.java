6e5938f2ae18c75b64f5061d552fd5cc77399ab0
llsrrll96 <llsrrll96@gmail.com>
2024-01-29 21:40:26 +0900

    update: 패키지로 예외처리

--- petlog-api/src/main/java/com/ppp/api/auth/exception/ErrorCode.java
+++ petlog-api/src/main/java/com/ppp/api/auth/exception/ErrorCode.java
@@ -14,7 +14,11 @@ public enum ErrorCode {
     MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-0004","토큰 형식이 잘못됨"),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-0005","토큰 만료"),
     UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-0006","지원하지 않는 토큰 형식"),
-    ILLEGALARGUMENT_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-0007","JWT 클레임 문자열이 비어 있음");
+    ILLEGALARGUMENT_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-0007","JWT 클레임 문자열이 비어 있음"),
+
+    EXISTS_EMAIL(HttpStatus.CONFLICT, "AUTH-0001", "이메일이 존재합니다."),
+    NOTMATCH_PASSWORD(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-0002", "패스워드가 다릅니다.");
+
 
     private final HttpStatus status;
     private final String code;
--- petlog-api/src/main/java/com/ppp/api/user/exception/ErrorCode.java
+++ petlog-api/src/main/java/com/ppp/api/user/exception/ErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "e-0001", "사용자를 찾을 수 없습니다.");
+    NOT_FOUND_USER(HttpStatus.INTERNAL_SERVER_ERROR, "USER-0001", "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
