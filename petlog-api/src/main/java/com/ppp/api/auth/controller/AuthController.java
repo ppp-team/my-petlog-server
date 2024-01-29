67e9379d4ce76a5cdf84ffd465b8c8f792492bbe
llsrrll96 <llsrrll96@gmail.com>
2024-01-29 21:36:49 +0900

    update : controller 수정 #5 refreshToken만 헤더로 받게함

--- petlog-api/src/main/java/com/ppp/api/auth/controller/AuthController.java
+++ petlog-api/src/main/java/com/ppp/api/auth/controller/AuthController.java
@@ -1,12 +1,13 @@
 package com.ppp.api.auth.controller;
 
 import com.ppp.api.auth.service.AuthService;
-import com.ppp.api.user.dto.AuthenticationRequest;
+import com.ppp.api.user.dto.SigninDto;
 import com.ppp.api.user.dto.AuthenticationResponse;
 import com.ppp.api.user.dto.RegisterRequest;
 import com.ppp.common.exception.ErrorCode;
 import com.ppp.common.exception.TokenException;
 import com.ppp.common.security.jwt.JwtTokenProvider;
+import com.ppp.common.util.ApiResponse;
 import com.ppp.domain.user.User;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 // 인증 X
 @Slf4j
@@ -32,16 +31,12 @@ public class AuthController {
      * email. password
      */
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody RegisterRequest signupDto) throws Exception {
-        log.debug("########## signup");
-
-        // add check for email exists in DB
-//        if(memberService.existsByEmail(loginDto.getEmail())) {
-//            return new ResponseEntity<>("이메일이 존재합니다.",HttpStatus.BAD_REQUEST);
-//        }
-
+    public ResponseEntity<?> signup(@RequestBody RegisterRequest signupDto) {
         User user = authService.signup(signupDto);
-        if(user == null) return new ResponseEntity<>("회원가입 실패", HttpStatus.INTERNAL_SERVER_ERROR); // 후추 수정
+        if(user == null) {
+            ApiResponse<User> failResponse = ApiResponse.fail("fail", "회원가입에 실패했습니다!");
+            return new ResponseEntity<>(failResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return ResponseEntity.noContent().build();
     }
 
@@ -49,20 +44,22 @@ public class AuthController {
      * 로그인시 토큰 발급
      */
     @PostMapping("/signin")
-    public ResponseEntity<AuthenticationResponse> signin(@RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(authService.signin(request));
+    public ResponseEntity<AuthenticationResponse> signin(@RequestBody SigninDto signinDto) {
+        return ResponseEntity.ok(authService.signin(signinDto));
     }
 
     /**
-     * 로그아웃, refresh 토큰 삭제
+     * 로그아웃, refresh 토큰 삭제 - 추후 구현..
      */
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request, @RequestHeader("Authorization") final String authHeader) {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) throw new TokenException(ErrorCode.NOT_FOUND_TOKEN);
-
-        final String refreshToken = authHeader.substring(7);
+    public ResponseEntity<Void> logout(
+            HttpServletRequest request,
+            @RequestHeader("refreshToken") String refreshToken
+    ) {
+        if (refreshToken == null) throw new TokenException(ErrorCode.NOT_FOUND_TOKEN);
         jwtTokenProvider.validateRefreshToken(refreshToken, request);
 
+        // 추후 레디스에서 토큰 삭제
 
         return ResponseEntity.noContent().build();
     }
@@ -70,13 +67,13 @@ public class AuthController {
     /**
      * AT 토큰 만료시 AT/RT 재발급
      * RT 갱신, AT
-     * https://github.com/ali-bouali/spring-boot-3-jwt-security/blob/main/src/main/java/com/alibou/security/auth/AuthenticationService.java
      */
     @PostMapping("/refresh-token")
-    public ResponseEntity<AuthenticationResponse> refreshToken(HttpServletRequest request, @RequestHeader("Authorization") final String authHeader) throws IOException {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) throw new TokenException(ErrorCode.NOT_FOUND_TOKEN);
-
-        final String refreshToken = authHeader.substring(7);
+    public ResponseEntity<AuthenticationResponse> regenerateToken(
+            HttpServletRequest request,
+            @RequestHeader("refreshToken") String refreshToken
+        ) {
+        if (refreshToken == null) throw new TokenException(ErrorCode.NOT_FOUND_TOKEN);
         jwtTokenProvider.validateRefreshToken(refreshToken, request);
 
         return ResponseEntity.ok(authService.regenerateToken(refreshToken));
