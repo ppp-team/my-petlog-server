cb656c5fb6c6ff351d0808fc27e1bb68a8186a76
haden <80521474+thdefn@users.noreply.github.com>
2024-01-31 22:39:17 +0900

    docs: write api documentation

--- petlog-api/src/main/java/com/ppp/api/diary/controller/DiaryController.java
+++ petlog-api/src/main/java/com/ppp/api/diary/controller/DiaryController.java
@@ -2,11 +2,19 @@ package com.ppp.api.diary.controller;
 
 import com.ppp.api.diary.dto.request.DiaryRequest;
 import com.ppp.api.diary.service.DiaryService;
+import com.ppp.api.exception.ExceptionResponse;
 import com.ppp.common.security.PrincipalDetails;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Tag(name = "Diary", description = "Diary APIs")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -21,9 +30,16 @@ import java.util.List;
 public class DiaryController {
     private final DiaryService diaryService;
 
-    @PostMapping(value = "/{petId}/diaries")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "일치하는 리소스 없음.", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "403", description = "해당 기록 공간에 대한 권한이 없음", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
+    @PostMapping(value = "/{petId}/diaries", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     private ResponseEntity<Void> createDiary(@PathVariable Long petId,
                                              @Valid @RequestPart DiaryRequest request,
+                                             @Parameter(description = "multipart form data 형식의 이미지를 등록해주세요.", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
                                              @Valid @RequestPart(required = false)
                                              @Size(max = 10, message = "이미지는 10개 이하로 첨부해주세요.") List<MultipartFile> images,
                                              @AuthenticationPrincipal PrincipalDetails principalDetails) {
@@ -31,9 +47,16 @@ public class DiaryController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/diaries/{diaryId}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "일치하는 리소스 없음.", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "403", description = "해당 기록 공간에 대한 권한이 없음", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
+    @PutMapping(value = "/diaries/{diaryId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     private ResponseEntity<Void> updateDiary(@PathVariable Long diaryId,
                                              @Valid @RequestPart DiaryRequest request,
+                                             @Parameter(description = "multipart form data 형식의 이미지를 등록해주세요.", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
                                              @Valid @RequestPart(required = false)
                                              @Size(max = 10, message = "이미지는 10개 이하로 첨부해주세요.") List<MultipartFile> images,
                                              @AuthenticationPrincipal PrincipalDetails principalDetails) {
@@ -41,7 +64,13 @@ public class DiaryController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/diaries/{diaryId}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "일치하는 리소스 없음.", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "403", description = "해당 기록 공간에 대한 권한이 없음", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
+    @DeleteMapping(value = "/diaries/{diaryId}")
     private ResponseEntity<Void> deleteDiary(@PathVariable Long diaryId,
                                              @AuthenticationPrincipal PrincipalDetails principalDetails) {
         diaryService.deleteDiary(principalDetails.getUser(), diaryId);
--- petlog-api/src/main/java/com/ppp/api/diary/dto/request/DiaryRequest.java
+++ petlog-api/src/main/java/com/ppp/api/diary/dto/request/DiaryRequest.java
@@ -1,5 +1,6 @@
 package com.ppp.api.diary.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -12,12 +13,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DiaryRequest {
+    @Schema(description = "제목", nullable = false, example = "오늘은 공원에서 산책을 했어요")
     @Size(min = 1, max = 255, message = "1자 이상 255자 이하의 제목을 입력해주세요.")
     private String title;
 
+    @Schema(description = "내용", nullable = false, example = "날씨가 좋아서 산책을 했답니다")
     @Size(min = 1, max = 5000, message = "1자 이상 5000자 이하의 내용을 입력해주세요.")
     private String content;
 
+    @Schema(description = "날짜", nullable = false, example = "2024-01-31")
     @PastOrPresent(message = "날짜를 확인해주세요.")
     private LocalDate date;
 }
+
