2aad02fa280111f98f0db2345fa99b225bdcf2c0
llsrrll96 <llsrrll96@gmail.com>
2024-02-05 16:17:37 +0900

    feat: Setting up swagger and adding a response code #20

--- petlog-api/src/main/java/com/ppp/api/user/controller/UserController.java
+++ petlog-api/src/main/java/com/ppp/api/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ppp.api.user.controller;
 
+import com.ppp.api.exception.ExceptionResponse;
 import com.ppp.api.user.dto.request.CheckRequest;
 import com.ppp.api.user.dto.response.UserResponse;
 import com.ppp.api.user.exception.ErrorCode;
@@ -7,6 +8,11 @@ import com.ppp.api.user.exception.UserException;
 import com.ppp.api.user.service.UserService;
 
 import com.ppp.common.security.PrincipalDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,22 +29,32 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
     private final UserService userService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "409", description = "닉네임 중복", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
     @PostMapping("/v1/users/check/nickname")
     public ResponseEntity<String> checkNickname(@RequestBody CheckRequest checkRequest) {
-        String nickname = checkRequest.getNickname();
-        if (userService.existsByNickname(nickname))
+        if (userService.existsByNickname(checkRequest.getNickname()))
             throw new UserException(ErrorCode.NICKNAME_DUPLICATION);
-        return ResponseEntity.ok(nickname);
+        return ResponseEntity.ok().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "409", description = "이메일 중복", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
     @PostMapping("/v1/users/check/email")
     public ResponseEntity<String> checkEmail(@RequestBody CheckRequest checkRequest) {
-        String email = checkRequest.getEmail();
-        if (userService.existsByEmail(email))
+        if (userService.existsByEmail(checkRequest.getEmail()))
             throw new UserException(ErrorCode.EMAIL_DUPLICATION);
-        return ResponseEntity.ok(email);
+        return ResponseEntity.ok().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "409", description = "이메일 중복", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
     @PostMapping("/v1/users/profile")
     public ResponseEntity<Void> createProfile(
             @RequestParam(required = false, value = "profileImage") MultipartFile profileImage,
@@ -49,6 +65,12 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "프로필 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Invalid password", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
     @PutMapping("/v1/users/profile")
     public ResponseEntity<Void> updateProfile(
             @RequestParam(required = false, value = "profileImage") MultipartFile profileImage,
@@ -60,6 +82,13 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "내 정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content =
+                    { @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = UserResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "User not found", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class))})
+    })
     @GetMapping("/v1/users/me")
     public ResponseEntity<UserResponse> displayMe(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return ResponseEntity.ok(userService.displayMe(principalDetails.getUser()));
--- petlog-api/src/main/java/com/ppp/api/user/exception/ErrorCode.java
+++ petlog-api/src/main/java/com/ppp/api/user/exception/ErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    NOT_FOUND_USER(HttpStatus.INTERNAL_SERVER_ERROR, "USER-0001", "사용자를 찾을 수 없습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "USER-0001", "사용자를 찾을 수 없습니다."),
     PROFILE_REGISTRATION_FAILED(HttpStatus.BAD_REQUEST, "USER-0002", "프로필 등록에 실패했습니다."),
     NICKNAME_DUPLICATION(HttpStatus.CONFLICT, "USER-0003", "닉네임 중복"),
     EMAIL_DUPLICATION(HttpStatus.CONFLICT, "USER-0004", "이메일 중복"),
