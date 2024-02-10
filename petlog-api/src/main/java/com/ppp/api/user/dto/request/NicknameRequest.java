991409e3b83085feead37764cacac9ab716d2320
llsrrll96 <llsrrll96@gmail.com>
2024-02-10 14:49:15 +0900

    chore: add swagger validation

--- petlog-api/src/main/java/com/ppp/api/user/dto/request/NicknameRequest.java
+++ petlog-api/src/main/java/com/ppp/api/user/dto/request/NicknameRequest.java
@@ -1,8 +1,10 @@
 package com.ppp.api.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class NicknameRequest {
+    @Schema(description = "닉네임", nullable = false)
     private String nickname;
 }
