b2d819e2e1ae4d2d2ad3d5d10541f6b9b297edd3
haden <80521474+thdefn@users.noreply.github.com>
2024-02-10 12:50:11 +0900

    hotfix: fix the error that could not connect elastic search on develop server

---
+++ petlog-domain/src/main/java/com/ppp/domain/common/config/ElasticSearchConfig.java
@@ -0,0 +1,9 @@
+package com.ppp.domain.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig {
+}
