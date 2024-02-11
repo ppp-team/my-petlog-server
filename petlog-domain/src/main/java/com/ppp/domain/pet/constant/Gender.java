4a1126242ecfe98b4ec948d803b9ad4a07d158a3
llsrrll96 <llsrrll96@gmail.com>
2024-02-11 21:54:02 +0900

    refactor: remove value field

--- petlog-domain/src/main/java/com/ppp/domain/pet/constant/Gender.java
+++ petlog-domain/src/main/java/com/ppp/domain/pet/constant/Gender.java
@@ -1,14 +1,9 @@
 package com.ppp.domain.pet.constant;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Gender {
-
-    MALE("MALE"),
-    FEMALE("FEMALE");
-
-    @Getter
-    private final String value;
+    MALE,
+    FEMALE;
 }
--- petlog-domain/src/main/java/com/ppp/domain/pet/constant/RepStatus.java
+++ petlog-domain/src/main/java/com/ppp/domain/pet/constant/RepStatus.java
@@ -2,16 +2,6 @@ package com.ppp.domain.pet.constant;
 
 // 사용자 대표 반려동물
 public enum RepStatus {
-    NORMAL("normal"),
-    REPRESENTATIVE("representative");
-
-    private final String value;
-
-    RepStatus(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    NORMAL,
+    REPRESENTATIVE;
 }
