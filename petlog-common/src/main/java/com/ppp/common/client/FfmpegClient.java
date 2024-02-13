27a7dd92771ed81ceac77fa793bd21af0e902bb6
haden <80521474+thdefn@users.noreply.github.com>
2024-02-13 12:21:08 +0900

    refactor: change to use interface's default method

--- petlog-common/src/main/java/com/ppp/common/client/FfmpegClient.java
+++ petlog-common/src/main/java/com/ppp/common/client/FfmpegClient.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
-import org.apache.commons.io.FileUtils;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,10 +15,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.List;
 
-import static com.ppp.common.exception.ErrorCode.*;
+import static com.ppp.common.exception.ErrorCode.FILE_UPLOAD_FAILED;
 
 @Component
 @Slf4j
@@ -29,11 +25,8 @@ public class FfmpegClient implements VideoConvertClient {
     private final FFmpegExecutor fFmpegExecutor;
 
     @Override
-    public String resolution(MultipartFile file) {
+    public String compress(MultipartFile file) {
         try {
-            if (file.isEmpty())
-                throw new FileException(NOT_VALID_FILE);
-
             File dirToBeSaved = new File(DEFAULT_PATH + "/" + LocalDateTime.now().format(dateTimeFormatter));
             if (!dirToBeSaved.exists()) dirToBeSaved.mkdirs();
             Path inputPath = Paths.get(dirToBeSaved.getPath(), file.getOriginalFilename());
@@ -56,23 +49,6 @@ public class FfmpegClient implements VideoConvertClient {
             return outputPath.toString();
         } catch (IOException e) {
             throw new FileException(FILE_UPLOAD_FAILED);
-        }
-    }
-
-    @Scheduled(cron = "0 0 0/3 * * *")
-    @Override
-    public void deleteDirectoryCreatedBeforeThreeHours() {
-        try {
-            Path targetPath = Path.of(DEFAULT_PATH + "/" + LocalDateTime.now().minusHours(3).format(dateTimeFormatter));
-            List<File> directoriesToBeDeleted = Files.walk(Path.of(DEFAULT_PATH), 1)
-                    .sorted(Comparator.naturalOrder())
-                    .filter(path -> path.compareTo(targetPath) <= 0)
-                    .map(Path::toFile)
-                    .toList();
-            for (File dir : directoriesToBeDeleted)
-                FileUtils.deleteDirectory(dir);
-        } catch (IOException e) {
-            log.error("Class : {}, Code : {}, Message : {}", getClass(), FILE_CLEAN_JOB_FAILED.getCode(), FILE_CLEAN_JOB_FAILED.getMessage());
         }
     }
 }
--- petlog-common/src/main/java/com/ppp/common/client/VideoConvertClient.java
+++ petlog-common/src/main/java/com/ppp/common/client/VideoConvertClient.java
@@ -1,16 +1,42 @@
 package com.ppp.common.client;
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+
+import static com.ppp.common.exception.ErrorCode.FILE_CLEAN_JOB_FAILED;
 
 
 public interface VideoConvertClient {
-    String resolution(MultipartFile file);
-
-    void deleteDirectoryCreatedBeforeThreeHours();
-
+    Logger log = LoggerFactory.getLogger(VideoConvertClient.class);
     String DEFAULT_PATH = "temp/encoded";
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHH");
 
+    String compress(MultipartFile file);
+
+    default void deleteTempVideoCreatedBefore(int hour) {
+        try {
+            Path targetPath = Path.of(DEFAULT_PATH + "/" + LocalDateTime.now().minusHours(hour).format(dateTimeFormatter));
+            List<File> directoriesToBeDeleted = Files.walk(Path.of(DEFAULT_PATH), 1)
+                    .sorted(Comparator.naturalOrder())
+                    .filter(path -> path.compareTo(targetPath) <= 0)
+                    .map(Path::toFile)
+                    .toList();
+            for (File dir : directoriesToBeDeleted)
+                FileUtils.deleteDirectory(dir);
+        } catch (IOException e) {
+            log.error("Class : {}, Code : {}, Message : {}", getClass(), FILE_CLEAN_JOB_FAILED.getCode(), FILE_CLEAN_JOB_FAILED.getMessage());
+        }
+    }
+    ;
 }
