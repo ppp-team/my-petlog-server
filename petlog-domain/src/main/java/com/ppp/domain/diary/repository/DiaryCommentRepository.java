b49bfdbde4a04e2105c1d6fa833090d5f9f80d26
haden <80521474+thdefn@users.noreply.github.com>
2024-02-19 18:08:00 +0900

    fix: change diary comment response sorting type to descending

--- petlog-domain/src/main/java/com/ppp/domain/diary/repository/DiaryCommentRepository.java
+++ petlog-domain/src/main/java/com/ppp/domain/diary/repository/DiaryCommentRepository.java
@@ -4,6 +4,7 @@ import com.ppp.domain.diary.Diary;
 import com.ppp.domain.diary.DiaryComment;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +16,7 @@ import java.util.Optional;
 public interface DiaryCommentRepository extends JpaRepository<DiaryComment, Long> {
     Optional<DiaryComment> findByIdAndIsDeletedFalse(Long id);
 
+    @EntityGraph(attributePaths = {"user"}, type = EntityGraph.EntityGraphType.FETCH)
     Slice<DiaryComment> findByDiaryAndIsDeletedFalse(Diary diary, PageRequest request);
 
     boolean existsByIdAndIsDeletedFalse(Long id);
