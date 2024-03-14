8b7f9d99b0f6ff021f8f4341774e37e6ec587541
llsrrll96 <llsrrll96@gmail.com>
2024-03-14 10:47:31 +0900

    chore : modify title

--- petlog-api/src/main/java/com/ppp/api/auth/service/EmailService.java
+++ petlog-api/src/main/java/com/ppp/api/auth/service/EmailService.java
@@ -16,6 +16,8 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
     private static final String senderEmail = "mypetlog.auth@gmail.com";
+    private static final String emailAuthTitle = "[마이펫로그] 이메일 인증 코드를 확인해 주세요";
+
 
     public int createNumber() {
         return (int)(Math.random() * (90000)) + 100000;
@@ -23,23 +25,23 @@ public class EmailService {
 
     public MimeMessage createEmailForm(String mail, int number){
         MimeMessage message = javaMailSender.createMimeMessage();
-        String title = "[마이펫로그] 이메일 인증 코드를 확인해 주세요";
         try {
             message.setFrom(senderEmail);
+            message.setSubject(emailAuthTitle);
             message.setRecipients(MimeMessage.RecipientType.TO, mail);
-            message.setSubject(title);
             String body =
                     "<div style=\"letter-spacing: -0.025em; padding: 28px; max-width: 374px; margin: 16px auto; border: 1px solid rgba(0,0,0,.06); border-radius: 8px;\">\n" +
                     "\n" +
                     "    <div style=\"letter-spacing: -0.025em; padding: 13px 0; height: 38px;\">\n" +
                     "    </div>\n" +
                     "<div style=\"letter-spacing: -0.025em; box-sizing: border-box; display: block; Margin: 0 auto;\">\n" +
-                    "    <h1 style=\"letter-spacing: -0.025em; margin-bottom: 30px; width: 100%; font-size: 28px; line-height: 38px; color: #1D1D1D; font-weight: bold; margin: 16px 20px 0 0; white-space: pre-line; word-break: break-word; padding-bottom: 24px;\">이메일 인증 코드를\n" +
-                    "확인해 주세요</h1>\n" +
+                    "    <h1 style=\"letter-spacing: -0.025em; margin-bottom: 30px; width: 100%; font-size: 28px; line-height: 38px; color: #1D1D1D; font-weight: bold; margin: 16px 20px 0 0; white-space: pre-line; word-break: break-word; padding-bottom: 24px;\">이메일 인증 코드를\n확인해 주세요</h1>\n" +
                     "    <div style=\"letter-spacing: -0.025em; color: #545454; font-size: 15px; line-height: 24px;\">\n" +
                     "        <p style=\"letter-spacing: -0.025em; font-size: 15px; font-weight: 400; font-style: normal; margin: 0; padding-bottom: 8px; color: #555555; white-space: pre-line;\">아래 인증 코드를 회원가입 페이지에 입력해 주세요</p>\n" +
                     "        <div style=\"letter-spacing: -0.025em; padding: 40px 0 20px 0;\">\n" +
-                    "            <section style=\"letter-spacing: -0.025em; background: rgba(153, 153, 153, 0.15); border-radius: 8px; font-weight: 500; font-size: 24px; line-height: 30px; text-align: center; padding: 16px 32px;\">"+number+"</section>\n" +
+                    "            <section style=\"letter-spacing: -0.025em; background: rgba(153, 153, 153, 0.15); border-radius: 8px; font-weight: 500; font-size: 24px; line-height: 30px; text-align: center; padding: 16px 32px;\">"
+                                    +number+
+                                "</section>\n" +
                     "        </div>\n" +
                     "    </div>\n" +
                     "    <div style=\"letter-spacing: -0.025em; margin: 0; padding: 0; word-break: break-word;\">\n" +
