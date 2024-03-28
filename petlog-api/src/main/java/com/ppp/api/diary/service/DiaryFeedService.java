6ed63173b5f191ae043d439e8c21e632f2e44d23
haden <80521474+thdefn@users.noreply.github.com>
2024-03-28 15:33:57 +0900

    fix: change diary feed logic to reflect subscribed pet diary size

--- petlog-api/src/main/java/com/ppp/api/diary/service/DiaryFeedService.java
+++ petlog-api/src/main/java/com/ppp/api/diary/service/DiaryFeedService.java
@@ -3,18 +3,22 @@ package com.ppp.api.diary.service;
 import com.ppp.api.diary.dto.response.DiaryFeedResponse;
 import com.ppp.api.subscription.dto.transfer.SubscriptionInfoDto;
 import com.ppp.api.subscription.service.SubscriptionService;
+import com.ppp.domain.diary.dto.PetDiaryDto;
 import com.ppp.domain.diary.repository.DiaryQuerydslRepository;
 import com.ppp.domain.user.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class DiaryFeedService {
 
     private final DiaryQuerydslRepository diaryQuerydslRepository;
@@ -24,23 +28,32 @@ public class DiaryFeedService {
 
     public Set<DiaryFeedResponse> retrieveDiaryFeed(User user, int page, int size) {
         SubscriptionInfoDto subscriptionInfo = subscriptionService.getUsersSubscriptionInfo(user.getId());
-        List<DiaryFeedResponse> subscribedPetsDiaries = diaryQuerydslRepository
-                .findSubscribedPetsDiariesBySubscription(subscriptionInfo.subscribedPetIds(), PageRequest.of(page, size / 2))
-                .stream().map(dto -> DiaryFeedResponse.from(dto,
-                        diaryCommentRedisService.getDiaryCommentCountByDiaryId(dto.getDiaryId()),
-                        diaryRedisService.isLikeExistByDiaryIdAndUserId(dto.getDiaryId(), user.getId()),
-                        diaryRedisService.getLikeCountByDiaryId(dto.getDiaryId()),
-                        true
-                )).toList();
-        List<DiaryFeedResponse> randomPetDiaries = diaryQuerydslRepository.findRandomPetsDiaries(subscriptionInfo.blockedPetIds(), PageRequest.of(page, size - size / 2))
-                .stream().map(dto -> DiaryFeedResponse.from(dto,
-                        diaryCommentRedisService.getDiaryCommentCountByDiaryId(dto.getDiaryId()),
-                        diaryRedisService.isLikeExistByDiaryIdAndUserId(dto.getDiaryId(), user.getId()),
-                        diaryRedisService.getLikeCountByDiaryId(dto.getDiaryId()),
-                        subscriptionInfo.subscribedPetIds().contains(dto.getPetId())
-                )).toList();
+
+        List<DiaryFeedResponse> subscribedPetsDiaries = new ArrayList<>();
+        if (!subscriptionInfo.subscribedPetIds().isEmpty()) {
+            subscribedPetsDiaries = diaryQuerydslRepository
+                    .findSubscribedPetsDiariesBySubscription(subscriptionInfo.subscribedPetIds(), PageRequest.of(page, size / 2))
+                    .stream().map(dto -> toDiaryFeedResponse(dto, user.getId(), subscriptionInfo.subscribedPetIds())).toList();
+        }
+        List<DiaryFeedResponse> randomPetDiaries = diaryQuerydslRepository.findRandomPetsDiaries(subscriptionInfo.blockedPetIds(), PageRequest.of(page, size - subscribedPetsDiaries.size()))
+                .stream().map(dto -> toDiaryFeedResponse(dto, user.getId())).toList();
         Set<DiaryFeedResponse> response = new HashSet<>(subscribedPetsDiaries);
         response.addAll(randomPetDiaries);
         return response;
+    }
+
+    private DiaryFeedResponse toDiaryFeedResponse(PetDiaryDto dto, String userId, Set<Long> subscribedPetIds) {
+        return DiaryFeedResponse.from(dto,
+                diaryCommentRedisService.getDiaryCommentCountByDiaryId(dto.getDiaryId()),
+                diaryRedisService.isLikeExistByDiaryIdAndUserId(dto.getDiaryId(), userId),
+                diaryRedisService.getLikeCountByDiaryId(dto.getDiaryId()),
+                subscribedPetIds.contains(dto.getPetId()));
+    }
+
+    private DiaryFeedResponse toDiaryFeedResponse(PetDiaryDto dto, String userId) {
+        return DiaryFeedResponse.from(dto,
+                diaryCommentRedisService.getDiaryCommentCountByDiaryId(dto.getDiaryId()),
+                diaryRedisService.isLikeExistByDiaryIdAndUserId(dto.getDiaryId(), userId),
+                diaryRedisService.getLikeCountByDiaryId(dto.getDiaryId()));
     }
 }
